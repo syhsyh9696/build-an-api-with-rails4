2ba220cc6763f4d95c8ea6ed2c2403fa5eb042f7
syhsyh9696 <sunyuanhao123456@gmail.com>
2017-08-14 00:58:04 +0000

    Add gem pundit

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SessionsController < Api::V1::BaseController
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+
+  def create
+    @user = User.find_by(email: create_params[:email])
+    if @user && @user.authenticate(create_params[:password])
+      self.current_user = @user
+    else
+      return api_error(status: 401)
+    end
+  end
+
+private
+
+  def create_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
